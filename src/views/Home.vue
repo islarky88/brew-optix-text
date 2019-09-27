3f42f3fb1edeb7328c998ff45a51de35d7a5d797
Islarky Gwapito <islarky88@gmail.com>
2019-09-28 03:15:34 +0800

    fetch api for brands

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,12 @@
     <h1>Brands List</h1>
     <v-container>
       <v-row>
-        <v-col cols="12" sm="6" lg="3">
+        <v-col v-for="(item, i) in $store.state.storeItems"
+          :key="i"
+          cols="12"
+          sm="6"
+          lg="3"
+        >
           <v-card>
             <v-card-title primary-title>
               Brand
@@ -18,18 +23,44 @@
         </v-col>
       </v-row>
     </v-container>
+
+    {{ brands }}
   </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld';
-
+  import axios from 'axios';
   export default {
     components: {
 
     },
+    data() {
+      return {
+        brands: [],
+      };
+    },
     mounted() {
-      alert('hello');
+      // console.log(process.env.BASE_API_URL);
+      this.fetchBrands();
+    },
+    methods: {
+      async fetchBrands() {
+
+        let url = 'http://private-anon-e2d1d383a9-brewoptixv2.apiary-mock.com/brands';
+
+        try {
+          let { data } = await axios.get(url);
+
+          if (data.length === 0) {
+            throw new Error('Post not found.');
+          }
+
+          this.brands = data;
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.log(e);
+        }
+      },
     },
   };
 </script>
