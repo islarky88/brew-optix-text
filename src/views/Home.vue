f9d9339f65fc776bc1d132b29a5732c7da99d1ff
Islarky Gwapito <islarky88@gmail.com>
2019-09-28 02:57:59 +0800

    place hlder for brands in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <HelloWorld ></HelloWorld>
+  <v-container>
+    <h1>Brands List</h1>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" lg="3">
+          <v-card>
+            <v-card-title primary-title>
+              Brand
+            </v-card-title>
+            <v-card-text>
+              this is a text
+            </v-card-text>
+            <v-card-actions>
+              See Details
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
@@ -7,7 +26,10 @@
 
   export default {
     components: {
-      HelloWorld,
+
+    },
+    mounted() {
+      alert('hello');
     },
   };
 </script>
